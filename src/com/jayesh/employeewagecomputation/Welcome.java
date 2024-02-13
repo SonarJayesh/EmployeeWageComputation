73bfd72967a0c0d332da829a1d35845bb02c9802
SonarJayesh <jayeshsonar2824@gmail.com>
2024-02-13 15:03:38 +0530

    [MODIFIED] : YOGESH UC-1 complete for Employee Attendence

--- src/com/jayesh/employeewagecomputation/Welcome.java
+++ src/com/jayesh/employeewagecomputation/Welcome.java
@@ -1,11 +1,31 @@
 package com.jayesh.employeewagecomputation;
 
+import java.util.Scanner;
+
 public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Welcome to Employee Wage Computation");
+		
+		int attendance=0;
+		
+		System.out.print("Enter Attendance [ 0 for Absend / 1 for Present  ] :");
+		
+		Scanner obj =new Scanner(System.in);
+		attendance=obj.nextInt();
+		
+		if(attendance==0)
+		{
+			System.out.println("Employee is Absent");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
+		
+		
 	}
 
 }
